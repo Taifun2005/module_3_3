ed283b54b7a6fbb9f19784c63314d92a53a364b4
Taifun2005 <lider2005@yandex.ru>
2024-08-18 11:27:12 +1100

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -1,17 +1,17 @@
-def print_params(a = 1, b = 'строка', c = True):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
 print_params()
-print_params(a ='test', b = 'первая', c ='строчка')
-print_params(a ='строчка', b = 'вторая')
+print_params(a='test', b='первая', c='строчка')
+print_params(a='строчка', b='вторая')
 print_params(b=25)
 print_params(c=[1, 2, 3])
 values_list = ('Тест', True, 87)
-values_dict ={"a":'test', "b":True, "c":78}
+values_dict = {"a": 'test', "b": True, "c": 78}
 print_params(*values_list)
 print_params(**values_dict)
 values_list_2 = ('Тест', 100)
 print_params(*values_list_2, 42)
-values_list_2 = [54.32, 'Строка' ]
+values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 42)
